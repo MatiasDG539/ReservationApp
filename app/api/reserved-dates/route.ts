3670f6b005b5065d0f0ba8a67620c086965ed06a
MatiasDG539 <gutierrezmatiasdaniel539@gmail.com>
2024-09-07 20:15:53 -0300

    Feat: Prevent the reservation of two equal dates at the same place

---
+++ app/api/reserved-dates/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import db from "../../config/db";
+
+export async function POST(request: Request) {
+  try {
+    const data = await request.json();
+    const { place } = data; // Obtener el lugar del cuerpo de la solicitud
+
+    if (!place) {
+      return NextResponse.json(
+        { message: "El parámetro 'place' es requerido" },
+        { status: 400 }
+      );
+    }
+
+    const [results] = await db.query(
+      `SELECT a.reservationDate
+       FROM applicants a
+       JOIN places p ON a.place = p.name
+       WHERE p.name = ? AND a.reservationStatus IN ('Pending', 'Reservado', 'Esperando pago')`,
+      [place]
+    );
+
+    return NextResponse.json(results);
+  } catch (error) {
+    console.error("Error al realizar la consulta:", error);
+    return NextResponse.json(
+      { message: "Error interno del servidor" },
+      { status: 500 }
+    );
+  }
+}
