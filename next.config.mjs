ec3c003c3eecfcfcecfa58e123350a593396b39a
MatiasDG539 <gutierrezmatiasdaniel539@gmail.com>
2024-10-21 19:36:15 -0300

    Feat: Dockerize the application to facilitate deployment and improve portability

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+  experimental: {
+    appDir: true, // Habilita el App Router si no lo tienes habilitado ya
+  },
+  webpack: (config) => {
+    // Configuración de alias
+    config.resolve.alias["@"] = path.resolve(process.cwd(), "app"); // Apunta a la carpeta 'app'
+    return config;
+  },
+};
 
 export default nextConfig;
